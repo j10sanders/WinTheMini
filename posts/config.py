2fa677fea786806c73f772db09dfbaf0017ff654
j10sanders <jps458@nyu.edu>
2016-04-16 17:04:20 +0000

    Initial commit

--- posts/config.py
+++
@@ -1,7 +0,0 @@
-class DevelopmentConfig(object):
-    DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/posts"
-    DEBUG = True
-
-class TestingConfig(object):
-    DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/posts-test"
-    DEBUG = True
--- posts/decorators.py
+++
@@ -1,5 +0,0 @@
-import json
-from functools import wraps
-
-from flask import request, Response
-
