9dffa42b89a2f2b4dfc312846d5e0bf605e0834d
j10sanders <jonsandersss@gmail.com>
2016-04-20 01:42:55 +0000

    added pagination

--- crossword/views.py
+++ crossword/views.py
@@ -3,11 +3,31 @@ from flask import render_template
 from . import app
 from .database import session, Entry
 
+PAGINATE_BY = 10
+
 @app.route("/")
-def entries():
+@app.route("/page/<int:page>")
+def entries(page=1):
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Entry).count()
+
+    start = page_index * PAGINATE_BY
+    end = start + PAGINATE_BY
+
+    total_pages = (count - 1) / PAGINATE_BY + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
-    entries = entries.all()
+    entries = entries[start:end]
+
     return render_template("entries.html",
-        entries=entries
+        entries=entries,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
     )
