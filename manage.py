9b5ae4419b62a614e45b38fabe88df8db6804565
j10sanders <jonsandersss@gmail.com>
2016-05-22 16:02:18 +0000

    getting ready for heroku

--- manage.py
+++ manage.py
@@ -18,12 +18,12 @@ def run():
     
 @manager.command
 def seed():
-    content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    #content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
-    for i in range(25):
+    for i in range(1):
         entry = Entry(
-            title="Test Entry #{}".format(i),
-            content=content
+            title="144",
+            content="meh"
         )
         session.add(entry)
     session.commit()
