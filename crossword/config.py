da9e4d15d447c6a8cfa1a3c1533114e210f72531
j10sanders <jonsandersss@gmail.com>
2016-05-26 19:44:04 +0000

    getting rid of extra links, back to test local

--- crossword/config.py
+++ crossword/config.py
@@ -1,7 +1,12 @@
 import os
 class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/crossword"
+    DEBUG = True
+    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
+
+
+'''import os
+class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
     DEBUG = True
-    SECRET_KEY = "Not secret"
-
-
+    SECRET_KEY = "Not secret"'''
