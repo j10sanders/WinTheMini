76fd33bc8795135885b18d197c31b97c82d2fb63
j10sanders <jonsandersss@gmail.com>
2016-06-15 00:30:03 +0000

    for heroku

--- crossword/config.py
+++ crossword/config.py
@@ -1,6 +1,6 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/crossword"
+    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
     DEBUG = True
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
 
