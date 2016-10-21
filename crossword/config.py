e8d499dd2db5d228ff86076e565fcc3e45b13695
j10sanders <jonsandersss@gmail.com>
2016-10-21 15:27:16 +0000

    fixing for heroku

--- crossword/config.py
+++ crossword/config.py
@@ -1,7 +1,7 @@
 import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
   
   
