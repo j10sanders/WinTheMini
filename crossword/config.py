b27b0fcf84857f95d0210f0b82afbaed31b2ff4c
j10sanders <jonsandersss@gmail.com>
2016-10-29 21:04:26 +0000

    travis is bein finiky

--- crossword/config.py
+++ crossword/config.py
@@ -1,14 +1,14 @@
 import os
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = True
-    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
-
 class TravisConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/crossword"
     DEBUG = False
     SECRET_KEY = "Not secret" 
     
+    
+class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
+    DEBUG = True
+    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
 
   
 
