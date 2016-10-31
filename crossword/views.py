cae2cc5bf3e221975c10d8dd768f37888d40944f
j10sanders <jonsandersss@gmail.com>
2016-10-31 16:20:50 +0000

    something is way wrong with the ordering.  taking this feature out for now

--- crossword/views.py
+++ crossword/views.py
@@ -143,7 +143,7 @@ def entries(selected_date = ("2017-6-7")):
         has_prev = True
         has_next = False
         
-    if has_prev == False:
+    '''if has_prev == False:
         sevendaysago = selected_date - timedelta(days=8)
         ywinner = session.query(Entry).filter(Entry.datetime >= sevendaysago, Entry.datetime < selected_date, Entry.day_rank == (1,)).order_by(Entry.datetime.desc())
         streak = 1
@@ -152,7 +152,7 @@ def entries(selected_date = ("2017-6-7")):
         for x in ywinner:
             print(x.user.name)
         while ywinnername == ywinner[streak].user.name:
-            streak += 1
+            streak += 1'''
         
         
             
