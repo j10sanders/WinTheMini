97087c7afebf580d39c63fb59f62a5281a95ad6c
j10sanders <jonsandersss@gmail.com>
2016-11-02 22:19:00 +0000

    email looks better.

--- crossword/views.py
+++ crossword/views.py
@@ -409,7 +409,7 @@ def pwresetrq_post():
     
     response = sparky.transmission.send(
         recipients=[request.form["email"]],
-        text='Please go to this URL to reset your password: https://salty-brook-41648.herokuapp.com' + url_for("pwreset_get",  id = (str(key))) + "\nEmail jonsandersss@gmail.com if this doesn't work for you.     \n\n'With a Crossword, we're challenging ourselves to make order out of chaos' - Will Shortz",
+        text="With a Crossword, we're challenging ourselves to make order out of chaos' - Will Shortz  \nPlease go to this URL to reset your password: https://salty-brook-41648.herokuapp.com" + url_for("pwreset_get",  id = (str(key))) + "\n Email jonsandersss@gmail.com if this doesn't work for you.",
         from_email=from_email,
         subject='Reset your password')
 
