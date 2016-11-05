6346bedeae922f05f37a51d4b5f4f85e4fd74f5a
j10sanders <jonsandersss@gmail.com>
2016-11-05 16:33:53 +0000

    changing streak calculation again

--- crossword/views.py
+++ crossword/views.py
@@ -147,13 +147,13 @@ def entries(selected_date = ("2017-10-7")):
         try:
             while selected_date == ywinner[i].datetime.replace(tzinfo=pytz.utc).astimezone(EST).date():
                 #print("i")
-                #print(i)
+                print(i)
                 i += 1
         except IndexError:
             i = i
         
         
-        streak = 0
+        streak = 1
         ywinnerid = ywinner[i].user.id
         ywinnername = ywinner[i].user.name
         #for count, x in enumerate(ywinner):
