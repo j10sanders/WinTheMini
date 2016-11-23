1cf193572eba415aed56f426d90ffae372e8d39e
j10sanders <jonsandersss@gmail.com>
2016-11-23 19:56:23 +0000

    that was silly

--- crossword/config.py
+++ crossword/config.py
@@ -7,7 +7,7 @@ class TravisConfig(object):
 
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
   
 
