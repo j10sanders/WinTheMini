7734d99cad95122062b12087502f4c4fbd515143
j10sanders <jonsandersss@gmail.com>
2016-11-23 18:05:45 +0000

    testing dropdown

--- crossword/config.py
+++ crossword/config.py
@@ -5,12 +5,10 @@ class TravisConfig(object):
     SECRET_KEY = "Not secret" 
     
     
-import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/newcrossword"
+    DEBUG = True
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
-
   
 
 
@@ -30,7 +28,7 @@ class DevelopmentConfig(object):
 import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
     
 import os
