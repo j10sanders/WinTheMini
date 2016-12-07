9613a5cc541884c677bc5b083bf2ab8d28b96b07
j10sanders <jonsandersss@gmail.com>
2016-12-07 17:04:04 +0000

    small cleanup

--- crossword/config.py
+++ crossword/config.py
@@ -3,23 +3,17 @@ class TravisConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/crossword"
     DEBUG = True
     SECRET_KEY = "Not secret" 
-    
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = False
-    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
-'''
+'''  
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/newcrossword"
     DEBUG = True
-  
+    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
+
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
     DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
   
-
-
 
 class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/crossword-test"
@@ -38,11 +32,6 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
     DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
-    
-import os
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/newcrossword"
-    DEBUG = True
-    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
+
 '''
 
