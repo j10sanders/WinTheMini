c508968c8747ee1398355bda548a559d70585a34
j10sanders <jonsandersss@gmail.com>
2017-01-02 00:02:41 +0000

    need this for ssl to not trigger

--- crossword/config.py
+++ crossword/config.py
@@ -1,15 +1,21 @@
 import os
 class TravisConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/crossword"
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = "Not secret" 
-    
+
+'''   
 class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/crossword-test"
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = "Not secret"
     
-'''
+class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/newcrossword"
+    DEBUG = True
+    SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
+    
+
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
     DEBUG = False
