7a86ff36094b3cc4b1756890d90d02cffc2c4247
j10sanders <jonsandersss@gmail.com>
2017-01-12 19:41:10 +0000

    little things

--- crossword/__init__.py
+++ crossword/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-#sslify = SSLify(app)
+sslify = SSLify(app)
 config_path = os.environ.get("CONFIG_PATH", "crossword.config.DevelopmentConfig")
 
 app.config.from_object(config_path)
--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -9,29 +9,35 @@ from splinter import Browser
 
 # Configure app to use the testing database
 os.environ["CONFIG_PATH"] = "crossword.config.TravisConfig"
-#os.environ["CONFIG_PATH"] = "crossword.config.TestingConfig"
+os.environ["CONFIG_PATH"] = "crossword.config.TestingConfig"
 
 from crossword import app
 from crossword.database import Base, engine, session, User
 
-'''I'm getting an integrity error. https://trac.edgewall.org/ticket/8575  Commenting out these tests until I can figure it out.
+'''I'm getting an integrity error. https://trac.edgewall.org/ticket/8575  Commenting out these tests until I can figure it out.'''
 class TestViews(unittest.TestCase):
     def setUp(self):
-        # Setup client
-        self.browser = Browser('phantomjs')
-        self.browser.driver.set_window_size(1024, 768)
+        """ Test setup """
+        self.browser = Browser("phantomjs")
 
-        # Setup DB
+        # Set up the tables in the database
         Base.metadata.create_all(engine)
-        # Create User
-        self.user = User(name='Alice', email='alice@example.com',
-                         password=generate_password_hash('test'))
+        
+        #session.execute("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users_name_key)+1)")
+
+        #success = simplejson.dumps({'success':'success',})
+
+        # Create an example user
+        self.user = User(name="AliceW", email="alicewl@example.com",
+                         password=generate_password_hash("test"))
         session.add(self.user)
         session.commit()
 
-        self.process = multiprocessing.Process(target=app.run)
+        self.process = multiprocessing.Process(target=app.run,
+                                               kwargs={"port": 8080})
         self.process.start()
         time.sleep(1)
+
 
     def tearDown(self):
         """ Test teardown """
@@ -43,30 +49,21 @@ class TestViews(unittest.TestCase):
         self.browser.quit()
 
     def test_login_correct(self):
-        self.browser.visit('http://127.0.0.1:5000/login')
-        self.browser.fill('email', 'alice@example.com')
-        self.browser.fill('password', 'test')
-        self.browser.find_by_css('button[type=submit]').click()
-        self.assertEqual(self.browser.url, 'http://127.0.0.1:5000/')
+        self.browser.visit("http://127.0.0.1:8080/login")
+        self.browser.fill("email", "alicewl@example.com")
+        self.browser.fill("password", "test")
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.assertEqual(self.browser.url, "http://127.0.0.1:8080/")
 
-    def test_authenticated_add_entry(self):
-        # do the login
-        self.browser.visit('http://127.0.0.1:5000/login')
-        self.browser.fill('email', 'alice@example.com')
-        self.browser.fill('password', 'test')
-        self.browser.find_by_css('button[type=submit]').click()
+    def test_login_incorrect(self):
+        self.browser.visit("http://127.0.0.1:8080/login")
+        self.browser.fill("email", "bob@example.com")
+        self.browser.fill("password", "test")
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.assertEqual(self.browser.url, "http://127.0.0.1:8080/login")
 
-        # navigate to the entry add form
-        self.browser.click_link_by_text('Add Entry')
-
-        # create a new entry
-        self.browser.fill('title', '40')
-        self.browser.fill('content', 'The Content')
-        self.browser.find_by_css('button[type=submit]').click()
-
-        # check for entry title in home
-        self.assertIn(40, [e.text for e in self.browser.find_by_css('.row h1')])
-'''
 
 if __name__ == '__main__':
     unittest.main()
