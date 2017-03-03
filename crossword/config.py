bcd00c41b3cbc2620dd372f09600971e978011d6
j10sanders <jonsandersss@gmail.com>
2017-03-03 00:24:05 +0000

    need logs for sparkmail

--- crossword/config.py
+++ crossword/config.py
@@ -1,7 +1,7 @@
 import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
     
 
