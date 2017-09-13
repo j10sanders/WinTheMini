a3374441a9f131737cb3bdc4012e186d8a87174a
j10sanders <jonsandersss@gmail.com>
2017-09-13 01:28:31 +0000

    trying again

--- crossword/config.py
+++ crossword/config.py
@@ -1,9 +1,8 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/crossword2"
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI =  os.environ["DATABASE_URL"]
+    DEBUG = False
     SECRET_KEY = os.environ.get("CROSSWORD_SECRET_KEY", os.urandom(12))
-
 '''
 class TravisConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/crossword"
